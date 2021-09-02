7002f0d0c3a6031984c98b264038446f230e9709
manbalboy <manbalboy@hanmail.net>
2021-09-02 23:28:42 +0900

    initialize java + gradle
    java gradle

---
+++ src/test/java/com/manbalboy/javaoop/MainTest.java
@@ -0,0 +1,12 @@
+package com.manbalboy.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main() {
+        Main.main(null);
+    }
+}
