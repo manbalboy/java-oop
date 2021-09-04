6948a26844d388634204d77ac45fbb3bb0fb1699
manbalboy <manbalboy@hanmail.net>
2021-09-04 19:53:24 +0900

    main 메서드를 이용한 소트사용
    main 메서드를 이용한 소트사용

--- src/main/java/com/manbalboy/javaoop/Main.java
+++ src/main/java/com/manbalboy/javaoop/Main.java
@@ -1,7 +1,12 @@
 package com.manbalboy.javaoop;
 
+import com.manbalboy.javaoop.logic.BubbleSort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello javads");
+        BubbleSort<String> sort = new BubbleSort<>();
+        System.out.println(sort.sort(Arrays.asList(args)));
     }
 }
